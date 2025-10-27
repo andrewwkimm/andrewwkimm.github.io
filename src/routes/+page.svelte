e66d2041746b39cc37a1cda05206b3e59f94ba00
andrewwkimm <andrewkimka@gmail.com>
2025-10-27 13:21:44 -0400

    Checkpoint to working homepage layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
 <script>
-  import "../app.css";
+  // No script needed for now
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="home">
+  <br />
+  <p>
+    I'm Andrew Kim, a New Yorker born and raised in Queens where I've spent most of my life.
+    Shortly after I was born, I was sent to temporarily live with my uncle and grandmother
+    in the suburbs of Daegu, South Korea before returning back to the states for elementary
+    school.
+  </p>
+
+  <p>
+    I am best reached through <a href="mailto:andrewkimka@gmail.com">email</a>.
+  </p>
+</div>
