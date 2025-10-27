1b7e4fbcde54312c84fa003cfdd8f71fa5fb6803
andrewwkimm <andrewkimka@gmail.com>
2025-10-27 16:26:31 -0400

    Render markdown on resume page

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  // import ResumeContent from '$lib/content/resume.md';
+  import ResumeContent from '../../content/resume.md';
+
+</script>
+
+<div class="resume-container">
+  <svelte:component this={ResumeContent} />
+</div>
