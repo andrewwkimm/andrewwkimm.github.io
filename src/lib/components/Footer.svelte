04278c53e1464aa92484e1bee930cea59132aafb
andrewwkimm <andrewkimka@gmail.com>
2025-10-27 14:59:41 -0400

    Set initial footer settings

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
   const socialLinks = [
     { platform: 'github', url: 'https://github.com/andrewwkimm', username: 'andrewwkimm' },
     { platform: 'instagram', url: 'https://www.instagram.com/andrewkim.io/', username: 'andrewkim.io' },
-    { platform: 'linkedin', url: 'https://www.linkedin.com/in/andrew-sungsoo-kim/', username: 'andrew-sungsoo-kim' },
+    { platform: 'linkedin', url: 'https://www.linkedin.com/in/andrewwkimm/', username: 'andrewwkimm' },
     { platform: 'twitter', url: 'https://twitter.com/andrewkimssk', username: 'andrewkimssk' }
   ];
 
@@ -41,9 +41,10 @@
     background-color: #fdfdfd;
   }
 
+  /* Changed grid ratio to 1fr 2fr to make the second column (description) wider */
   .footer-content {
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr 1.58fr;
     gap: 40px;
     margin-bottom: 20px;
   }
@@ -62,12 +63,12 @@
   }
 
   .social-media-list li {
-    margin-bottom: 5px;
+    /* No styles needed here, uses default block spacing */
   }
 
   .social-media-list a {
     color: #828282;
-    font-size: 15px;
+    font-size: 18px;
   }
 
   .social-media-list a:hover {
@@ -76,18 +77,19 @@
 
   .footer-col-2 p {
     color: #828282;
-    font-size: 15px;
+    font-size: 17px;
     line-height: 1.5;
   }
 
+  /* Removed border-top and adjusted padding to remove the line */
   .copyright {
     text-align: center;
-    padding-top: 15px;
-    border-top: 1px solid #e8e8e8;
+    padding-top: 10px;
+    padding-bottom:20px;
   }
 
   .copyright p {
     color: #828282;
-    font-size: 14px;
+    font-size: 15px;
   }
 </style>
