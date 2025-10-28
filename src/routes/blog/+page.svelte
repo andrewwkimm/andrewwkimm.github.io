2d8b2097113c088566aa6f6520773763a4cde266
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 03:19:06 -0400

    Add blog posts

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let data;
+</script>
+
+<div class="blog-list">
+  <h1>Blog</h1>
+  {#each data.posts as post}
+    <article>
+      <h2><a href="/blog/{post.slug}">{post.metadata.title}</a></h2>
+      <time>{post.metadata.date}</time>
+    </article>
+  {/each}
+</div>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let data;
+</script>
+
+<article>
+  <svelte:component this={data.content} />
+</article>
+
+<style>
+  article {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 2rem;
+  }
+</style>
