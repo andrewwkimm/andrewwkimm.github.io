28f04a0b30923c8a249570cdf17d47e49dbed637
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 12:25:27 -0400

    Set blog page and post settings

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,4 +2,10 @@
   export let data;
 </script>
 
-<svelte:component this={data.content} />
+<article class="prose max-w-none">
+  <h1 class="text-4xl mb-2">{data.metadata.title}</h1>
+
+  <time class="text-gray-500 text-base block mb-6">{data.metadata.date}</time>
+
+  <svelte:component this={data.content} />
+</article>
