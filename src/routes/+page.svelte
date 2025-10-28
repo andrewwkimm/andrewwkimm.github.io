2ff3d033e6ffa9fdde867f3a681a39dc96111102
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 07:33:48 -0400

    Configure layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,13 @@
   // No script needed for now
 </script>
 
-<div>
-  <p>
-    I'm Andrew Kim, a New Yorker born and raised in Queens where I've spent most of my life.
-    Shortly after I was born, I was sent to temporarily live with my uncle and grandmother
-    in the suburbs of Daegu, South Korea before returning back to the states for elementary
-    school.
-  </p>
+<p>
+  I'm Andrew Kim, a New Yorker born and raised in Queens where I've spent most of my life.
+  Shortly after I was born, I was sent to temporarily live with my uncle and grandmother
+  in the suburbs of Daegu, South Korea before returning back to the states for elementary
+  school.
+</p>
 
-  <p>
-    I am best reached through <a href="mailto:andrewkimka@gmail.com">email</a>.
-  </p>
-</div>
+<p>
+  I am best reached through <a href="mailto:andrewkimka@gmail.com">email</a>.
+</p>
