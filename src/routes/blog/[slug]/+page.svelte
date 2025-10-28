cc6afcf0d4d3fdd5ebd246e691f8ec295d8de6b4
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 03:43:14 -0400

    Change blog formatting

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,14 +2,4 @@
   export let data;
 </script>
 
-<article>
-  <svelte:component this={data.content} />
-</article>
-
-<style>
-  article {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 2rem;
-  }
-</style>
+<svelte:component this={data.content} />
