4c025b2d1e2f434ad2dffd523aed82681a1b9561
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 12:06:12 -0400

    Refactor readling list page

--- src/routes/reading_list/+page.svelte
+++ src/routes/reading_list/+page.svelte
@@ -1,7 +1,86 @@
-<script>
-  import ReadingList from '../../content/reading_list.md';
-</script>
 
-<div>
-  <svelte:component this={ReadingList} />
-</div>
+<h2>Books I've enjoyed or planning to read</h2>
+
+<br>
+
+<ul class="list-disc pl-7 marker:text-neutral-400 space-y-1">
+  <li><strong>40 Puzzles and Problems in Probability and Mathematical Statistics</strong> (Wolfgang Schwarz)</li>
+  <li><strong>A Death in Tokyo</strong> (Keigo Higashino)</li>
+  <li><strong>A Farewell to Alms</strong> (Gregory Clark)</li>
+  <li><strong>A Midsummer's Equation</strong> (Keigo Higashino)</li>
+  <li><strong>A Pattern Language: Towns, Buildings, Construction</strong> (Christopher Alexander)</li>
+  <li><strong>A Practical Guide To Quantitative Finance Interviews</strong> (Xinfeng Zhou)</li>
+  <li><strong>A Quiet Place</strong> (Matsumoto Seicho)</li>
+  <li><strong>Academically Adrift: Limited Learning on College Campuses</strong> (Richard Arum, Josipa Roksa)</li>
+  <li><strong>All She Was Worth</strong> (Miyuki Miyabe)</li>
+  <li><strong>And Then There Were None</strong> (Agatha Christie)</li>
+  <li><strong>Angela's Ashes: A Memoir</strong> (Frank McCourt)</li>
+  <li><strong>Anna Karenina</strong> (Leo Tolstoy)</li>
+  <li><strong>Bargaining for Advantage</strong> (G. Richard Shell)</li>
+  <li><strong>Cod: A Biography of the Fish that Changed the World</strong> (Mark Kurlansky)</li>
+  <li><strong>Confessions</strong> (Kanae Minato)</li>
+  <li><strong>Crime and Punishment</strong> (Fyodor Dostoevsky)</li>
+  <li><strong>Death in the House of Rain</strong> (Szu-Yen Lin)</li>
+  <li><strong>Don't Make Me Think</strong> (Steve Krug)</li>
+  <li><strong>Endless Night</strong> (Agatha Christie)</li>
+  <li><strong>Foreign Affairs</strong> (Alison Lurie)</li>
+  <li><strong>George Marshall: Defender of the Republic</strong></li>
+  <li><strong>Good-Bye to All That: An Autobiography</strong> (Robert Graves)</li>
+  <li><strong>Grotesque</strong> (Natsuo Kirino)</li>
+  <li><strong>How Computers Really Work: A Hands-On Guide to the Inner Workings of the Machine</strong> (Matthew Justice)</li>
+  <li><strong>How to Prove It</strong> (Daniel J. Velleman)</li>
+  <li><strong>How to Solve It</strong> (George Pólya)</li>
+  <li><strong>How to Write Science Fiction and Fantasy</strong> (Orson Scott Card)</li>
+  <li><strong>In the Miso Soup</strong> (Ryu Murakami)</li>
+  <li><strong>Introduction to the Theory of Econometrics</strong> (Jan Magnus)</li>
+  <li><strong>Journey Under the Midnight Sun</strong> (Keigo Higashino)</li>
+  <li><strong>Lolita</strong> (Vladimir Nabokov)</li>
+  <li><strong>Magpie Murders</strong> (Anthony Horowitz)</li>
+  <li><strong>Malice</strong> (Keigo Higashino)</li>
+  <li><strong>Meditations</strong> (Marcus Aurelius)</li>
+  <li><strong>Murder in the Crooked House</strong> (Soji Shimada)</li>
+  <li><strong>Murder of Roger Ackroyd</strong> (Agatha Christie)</li>
+  <li><strong>My Life as a Quant: Reflections on Physics and Finance</strong> (Emmanuel Derman)</li>
+  <li><strong>Naoko</strong> (Keigo Higashino)</li>
+  <li><strong>Newcomer: A Story of Destined Tokyo</strong> (Keigo Higashino)</li>
+  <li><strong>Numerical Matrix Analysis</strong> (Ilse Ipsen)</li>
+  <li><strong>On War</strong> (Carl von Clausewitz)</li>
+  <li><strong>On Writing Well</strong> (William Zinsser)</li>
+  <li><strong>Out</strong> (Natsuo Kirino)</li>
+  <li><strong>Penance</strong> (Kanae Minato)</li>
+  <li><strong>Points and Lines</strong> (Seicho Matsumoto)</li>
+  <li><strong>Problems and Snapshots from the World of Probability</strong> (Dennis Sandell, Gunnar Blom, and Lars Holst)</li>
+  <li><strong>Real World</strong> (Natsuo Kirino)</li>
+  <li><strong>Salvation of a Saint</strong> (Keigo Higashino)</li>
+  <li><strong>Sherlock Holmes: The Hound of the Baskervilles, A Study in Scarlet, The Valley of Fear, The Sign of Four</strong> (Arthur Conan Doyle)</li>
+  <li><strong>Shoe Dog</strong> (Phil Knight)</li>
+  <li><strong>Silent Parade: A Detective Galileo Novel</strong> (Keigo Higashino)</li>
+  <li><strong>Snow Country</strong> (Yasunari Kawabata)</li>
+  <li><strong>Survival of the Prettiest: The Science of Beauty</strong> (Nancy Etcoff)</li>
+  <li><strong>The Book of Evidence</strong> (John Banville)</li>
+  <li><strong>The Concepts and Practice of Mathematical Finance</strong> (Mark S. Joshi)</li>
+  <li><strong>The Decagon House Murders</strong> (Yukito Ayatsuji)</li>
+  <li><strong>The Devotion of Suspect X</strong> (Keigo Higashino)</li>
+  <li><strong>The Edogawa Rampo Reader</strong> (Edogawa Rampo)</li>
+  <li><strong>The Final Curtain</strong> (Keigo Higashino)</li>
+  <li><strong>The Goddess Chronicle</strong> (Natsuo Kirino)</li>
+  <li><strong>The Groves of Academe</strong> (Mary McCarthy)</li>
+  <li><strong>The Hollow Man</strong> (John Dickson Carr)</li>
+  <li><strong>The Honjin Murders</strong> (Seishi Yokomizo)</li>
+  <li><strong>The Inner Game of Tennis</strong> (W. Timothy Gallwey)</li>
+  <li><strong>The Lady Killer</strong> (Masako Togawa)</li>
+  <li><strong>The Lean Startup</strong> (Eric Ries)</li>
+  <li><strong>The Likeness</strong> (Tana French)</li>
+  <li><strong>The Little Book of Common Sense Investing: The Only Way to Guarantee Your Fair Share of Stock Market Returns</strong> (John C. Bogle)</li>
+  <li><strong>The Master Key</strong> (Masako Togawa)</li>
+  <li><strong>The Mill House Murders</strong> (Yukito Ayatsuji)</li>
+  <li><strong>The Miracles of the Namiya General Store</strong> (Keigo Higashino)</li>
+  <li><strong>The Murder of Roger Ackroyd</strong> (Agatha Christie)</li>
+  <li><strong>The Mystery of the Yellow Room</strong> (Gaston Leroux)</li>
+  <li><strong>The Name of the Game is a Kidnapping</strong> (Keigo Higashino)</li>
+  <li><strong>The Shadow University: The Betrayal Of Liberty On America's Campuses</strong> (Alan Charles Kors and Harvey Silverglate)</li>
+  <li><strong>The Splendid and the Vile: A Saga of Churchill, Family, and Defiance During the Blitz</strong></li>
+  <li><strong>The Tattoo Murder Case</strong> (Akimitsu Takagi)</li>
+  <li><strong>The Tokyo Zodiac Murders</strong> (Soji Shimada)</li>
+  <li><strong>The Wit and Wisdom of Charles T. Munger</strong> (Charlie Munger)</li>
+</ul>
