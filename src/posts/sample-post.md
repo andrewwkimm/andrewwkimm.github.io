789a9e1e0d2936cac42d48d23b32b534485073bb
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 03:43:14 -0400

    Change blog formatting

---
+++ src/posts/sample-post.md
@@ -0,0 +1,10 @@
+---
+title: "Another Example Post"
+date: "2025-11-01"
+---
+
+Text
+
+```bash
+make build
+```
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -2,8 +2,7 @@
   export let data;
 </script>
 
-<div class="blog-list">
-  <h1>Blog</h1>
+<div>
   {#each data.posts as post}
     <article>
       <h2><a href="/blog/{post.slug}">{post.metadata.title}</a></h2>
