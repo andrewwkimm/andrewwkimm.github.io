ef9c893ecfc423ebf3a8723bc5d60d1b49fc2aae
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 15:24:04 -0400

    Move plugin to dev dependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
 import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ".md"],
 
   preprocess: [
-    vitePreprocess(),
     mdsvex({
       extensions: ['.md']
     }),
