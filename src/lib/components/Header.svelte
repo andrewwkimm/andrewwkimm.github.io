edf647db487ad6be765ca4e05bdc1e7c3a8035bd
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 11:39:31 -0400

    Change hyper link styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,15 +7,32 @@
   ];
 </script>
 
-<header class="border-t-[5px] border-t-[var(--color-dark-gray)] border-b border-b-[var(--color-border)] min-h-[55.95px] leading-[60px] flex-shrink-0">
+<header
+  class="
+    border-t-[5px] border-t-[var(--color-dark-gray)]
+    border-b border-b-[var(--color-border)]
+    min-h-[55.95px] leading-[60px]
+    flex-shrink-0
+  "
+>
   <div class="wrapper flex justify-between items-center">
-    <a class="text-[31px] font-light tracking-tight text-[var(--color-dark-gray)] hover:no-underline" href="/">
+    <a
+      class="
+        text-[31px] font-light tracking-tight
+        text-[var(--color-dark-gray)]
+        no-underline hover:underline
+      "
+      href="/"
+    >
       Andrew Kim
     </a>
 
     <nav class="space-x-[30px]">
       {#each navigation as item}
-        <a class="text-[var(--color-text)] hover:no-underline" href={item.href}>
+        <a
+          class="text-[var(--color-text)] no-underline hover:underline"
+          href={item.href}
+        >
           {item.name}
         </a>
       {/each}
