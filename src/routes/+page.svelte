d2d3fdf6b6b5248aab9a31940f5f618a7a299f61
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 04:23:11 -0400

    Use global styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   // No script needed for now
 </script>
 
-<div class="home">
+<div>
   <p>
     I'm Andrew Kim, a New Yorker born and raised in Queens where I've spent most of my life.
     Shortly after I was born, I was sent to temporarily live with my uncle and grandmother
