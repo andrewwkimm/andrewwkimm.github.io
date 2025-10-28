36e388f6064c2f58cc6bd73bb5f0f92e19ce4463
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 12:47:03 -0400

    Revert global hyper link settings to only blog

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,10 +2,12 @@
   export let data;
 </script>
 
-<article class="prose max-w-none">
-  <h1 class="text-4xl mb-2">{data.metadata.title}</h1>
+<div class="blog">
+  <article class="prose max-w-none">
+    <h1 class="text-4xl font-medium mb-2">{data.metadata.title}</h1>
 
-  <time class="text-gray-500 text-base block mb-6">{data.metadata.date}</time>
+    <time class="text-gray-500 text-base font-normal block mb-6">{data.metadata.date}</time>
 
-  <svelte:component this={data.content} />
-</article>
+    <svelte:component this={data.content} />
+  </article>
+</div>
