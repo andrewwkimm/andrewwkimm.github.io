8761b096c87454d6518e4ecfecd630736c3c6653
andrewwkimm <andrewkimka@gmail.com>
2025-10-28 12:32:15 -0400

    Add space between text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
-<script>
-  // No script needed for now
-</script>
+<div class="space-y-4">
+  <p>
+    I'm Andrew Kim, a New Yorker born and raised in Queens where I've spent most of my life.
+    Shortly after I was born, I was sent to temporarily live with my uncle and grandmother
+    in the suburbs of Daegu, South Korea before returning back to the states for elementary
+    school.
+  </p>
 
-<p>
-  I'm Andrew Kim, a New Yorker born and raised in Queens where I've spent most of my life.
-  Shortly after I was born, I was sent to temporarily live with my uncle and grandmother
-  in the suburbs of Daegu, South Korea before returning back to the states for elementary
-  school.
-</p>
-
-<p>
-  I am best reached through <a href="mailto:andrewkimka@gmail.com">email</a>.
-</p>
+  <p>
+    I am best reached through <a href="mailto:andrewkimka@gmail.com">email</a>.
+  </p>
+</div>
