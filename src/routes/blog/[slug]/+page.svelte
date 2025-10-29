e300293b28b42e82886f9767bd20126c18350408
andrewwkimm <andrewkimka@gmail.com>
2025-10-29 10:37:49 -0400

    Change date size to normal

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -4,9 +4,18 @@
 
 <div class="blog">
   <article class="prose max-w-none">
-    <h1 class="text-3xl font-medium mb-2">{data.metadata.title}</h1>
+    <h1 class="
+      text-3xl
+      font-medium
+      mb-2"
+      >{data.metadata.title}
+    </h1>
 
-    <time class="text-gray-500 text-base font-normal block mb-6">{data.metadata.date}</time>
+    <time class="
+      text-[var(--color-gray)]
+      block mb-6"
+      >{data.metadata.date}
+    </time>
 
     <svelte:component this={data.content} />
   </article>
