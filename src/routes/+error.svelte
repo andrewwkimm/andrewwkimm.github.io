975fa42007c653ac01ec09a4591b29d82a3fe23a
andrewwkimm <andrewkimka@gmail.com>
2026-03-05 02:57:11 -0500

    Add basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<div class="space-y-4">
+  <h1>{$page.status}: {$page.error?.message ?? 'Something went wrong'}</h1>
+  <p><a href="/">Return home</a></p>
+</div>
