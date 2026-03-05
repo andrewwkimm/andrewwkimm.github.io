11328600a679ae25312e44a407f8de4e3e9d632a
andrewwkimm <andrewkimka@gmail.com>
2026-03-05 03:06:04 -0500

    Fix breaking build by importing PageData

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  export let data: PageData;
+</script>
+
 <div class="blog">
   {#each data.posts as post}
     <article class="mb-12">
       <time class="block mb-1 text-gray-500">{post.metadata.date}</time>
-
       <h2 class="text-3xl font-normal">
-        <a href="/blog/{post.slug}" class="hover:underline">{post.metadata.title}</a>
+        <a href="/blog/{post.slug}">{post.metadata.title}</a>
       </h2>
     </article>
   {/each}
